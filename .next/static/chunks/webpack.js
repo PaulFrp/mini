40f14b358fac8dd34c43980f5044681ff01595a6
Paul Frappier <paul.frappier@hotmail.fr>
2025-06-21 18:39:25 +0200

    fixed card size for phone screen

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7a7c860441a57ca8"; }
+/******/ 		__webpack_require__.h = function() { return "52ce1facf99766b4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
